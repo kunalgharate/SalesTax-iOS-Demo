24dc27a5263556ff5c4cee34fb39abea627188be
cvtkunalgharate <kunal.gharate@corevaluetech.com>
2021-10-25 14:57:55 +0530

    Sales tax using story board

--- SalesTax/ViewController.swift
+++ SalesTax/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var beforeTextPrice:Float = 0;
+    var saleTaxRate:Float = 0;
+    @IBOutlet weak var afterTextPriceTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func beforeTextPriceChanged(_ sender: UITextField) {
+        if let value = Float(sender.text!)
+        {
+            beforeTextPrice = value
+        }
+        else
+        {
+            beforeTextPrice = 0
+        }
+        
+    }
+    
+    
+    @IBAction func saleTaxRateChanged(_ sender: UITextField) {
+        
+        if let value = Float(sender.text!)
+        {
+            saleTaxRate = value
+        }
+        else
+        {
+            saleTaxRate = 0
+        }
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        
+        
+        let salestax = beforeTextPrice *
+        saleTaxRate / 100
+        
+        
+    let afterTaxPrice = beforeTextPrice + salestax
+        
+        afterTextPriceTextField.text = String(afterTaxPrice)
+    }
 }
 
